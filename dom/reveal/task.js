22d3243bf4f38f93ee4db85d18446db3b0a6c341
stormozov <web@stormozov.ru>
2025-03-04 15:41:11 +0300

    [ДЗ-3] Улучшил код показа элемента при скролле

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,35 +1,20 @@
 /**
- * Проверка видимости элемента
- * @param {HTMLElement} elem - HTML-элемент.
- * @param {number} threshold - Порог видимости. По умолчанию 0.
- */
-function isElementInViewport(elem, threshold) {
-  const rect = elem.getBoundingClientRect();
-  const windowHeight = window.innerHeight;
-
-  threshold = (typeof threshold === 'number') || 0;
-
-  return (
-    rect.top < windowHeight - threshold &&
-    rect.bottom > 0 + threshold
-  );
-}
-
-/**
  * Обновление видимости элементов
+ * 
+ * @param {NodeList} elements - Список элементов, к которым необходимо применить 
+ * класс активности.
  * @param {Object} params - Объект с параметрами.
  * @param {string} params.selector - Селектор элементов.
  * @param {string} params.activeClass - Активный класс элементов.
- * @param {boolean} params.removeClassOnExit - Удаление активного класса при выходе из зоны 
- * видимости. По умолчанию false.
- * @param {number} params.threshold - Порог видимости. По умолчанию 0.
+ * @param {boolean} params.removeClassOnExit - Нужно ли удалить активный класс при
+ * выходе из зоны видимости. По умолчанию false.
  */
-function updateVisibleElements({ selector, activeClass, removeClassOnExit, threshold }) {
-  const elements = document.querySelectorAll(selector);
-  const shouldRemoveClassOnExit = (typeof removeClassOnExit === 'boolean') || false;
-
+function updateVisibleElements(elements, { elements, activeClass, shouldRemoveClassOnExit }) {
   elements.forEach((elem) => {
-    if (isElementInViewport(elem, threshold)) {
+    const { innerHeight } = window;
+    const { top } = elem.getBoundingClientRect();
+    
+    if (top < innerHeight && top > 0) {
       elem.classList.add(activeClass);
     } else if (shouldRemoveClassOnExit) {
       elem.classList.remove(activeClass);
@@ -39,16 +24,17 @@ function updateVisibleElements({ selector, activeClass, removeClassOnExit, thres
 
 /**
  * Инициализация ScrollReveal
+ * 
  * @param {Object} params - Параметры.
  * @param {string} params.selector - Селектор элементов.
  * @param {string} params.activeClass - Активный класс элементов.
- * @param {boolean} params.removeClassOnExit - Удаление активного класса при выходе из зоны 
- * видимости. По умолчанию false.
- * @param {number} params.threshold - Порог видимости. По умолчанию 0.
+ * @param {boolean} params.removeClassOnExit - Нужно ли удалить активный класс при 
+ * выходе из зоны видимости. По умолчанию false.
  */
 function initScrollReveal(params) {
+  const elements = document.querySelectorAll(params.selector);
   document.addEventListener('scroll', () => {
-    requestAnimationFrame(() => updateVisibleElements(params));
+    requestAnimationFrame(() => updateVisibleElements(elements, params));
   });
 }
 
